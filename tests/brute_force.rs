f851701a7230f81ab97f4f594d175d7327295965
cavemanloverboy <caveycool@gmail.com>
2023-05-28 16:03:53 -0700

    add brute force periodic

--- tests/brute_force.rs
+++ tests/brute_force.rs
@@ -2,13 +2,13 @@ use std::error::Error;
 
 use bosque::{
     abacussummit::uncompressed::CP32,
-    tree::{into_tree, nearest_one, squared_euclidean, Index},
+    tree::{into_tree, nearest_one, nearest_one_periodic, squared_euclidean, Index},
 };
 use rand::{rngs::ThreadRng, Rng};
 use rayon::prelude::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
 const NDATA: usize = 10_000;
-const NQUERY: usize = 1_000_000;
+const NQUERY: usize = 100_000;
 
 #[test]
 fn test_brute_force() -> Result<(), Box<dyn Error>> {
@@ -42,6 +42,20 @@ fn test_brute_force() -> Result<(), Box<dyn Error>> {
         .enumerate()
         .for_each(|(i, q)| assert_eq!(results[i], brute_force(q, &data)));
 
+    // Query tree periodic
+    let results: Vec<_> = query
+        .par_iter()
+        .take(NQUERY / 100)
+        .map(|q| unsafe { nearest_one_periodic(&data, data.as_ptr(), q, 0) })
+        .collect();
+
+    // Brute force check periodic results
+    query
+        .iter()
+        .take(NQUERY / 100)
+        .enumerate()
+        .for_each(|(i, q)| assert_eq!(results[i], brute_force_periodic(q, &data), "failed on {i}"));
+
     Ok(())
 }
 
@@ -57,11 +71,31 @@ fn brute_force(q: &[f32; 3], data: &[[CP32; 3]]) -> (f32, usize) {
     let mut best_dist_sq = f32::MAX;
     let mut best = usize::MAX;
     for (d, i) in data.iter().zip(0..) {
-        let dist = squared_euclidean(d, q);
+        let dist_sq = squared_euclidean(d, q);
 
-        if dist < best_dist_sq {
-            best_dist_sq = dist;
+        if dist_sq < best_dist_sq {
+            best_dist_sq = dist_sq;
             best = i;
+        }
+    }
+
+    (best_dist_sq, best)
+}
+
+fn brute_force_periodic(q: &[f32; 3], data: &[[CP32; 3]]) -> (f32, usize) {
+    let mut best_dist_sq = f32::MAX;
+    let mut best = usize::MAX;
+    for (d, i) in data.iter().zip(0..) {
+        for img in 0..3_i32.pow(3) {
+            let image = [0_i32, 1, 2]
+                .map(|idx| q[idx as usize] + (((img / 3_i32.pow(idx as u32)) % 3) - 1) as f32);
+
+            let dist_sq = squared_euclidean(d, &image);
+
+            if dist_sq < best_dist_sq {
+                best_dist_sq = dist_sq;
+                best = i;
+            }
         }
     }
 
