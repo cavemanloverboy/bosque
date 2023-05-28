0cb24c9fe07d0995ca73abe36976b92e3e425f6c
cavemanloverboy <caveycool@gmail.com>
2023-05-28 16:25:45 -0700

    add nearest_k brute force test

--- tests/brute_force_k.rs
+++ tests/brute_force_k.rs
@@ -2,7 +2,7 @@ use std::{collections::BinaryHeap, error::Error};
 
 use bosque::{
     abacussummit::uncompressed::CP32,
-    tree::{into_tree, nearest_k, squared_euclidean, Index, F32},
+    tree::{into_tree, nearest_k, nearest_k_periodic, squared_euclidean, Index, F32},
 };
 use rand::{rngs::ThreadRng, Rng};
 use rayon::prelude::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
@@ -35,8 +35,7 @@ fn test_brute_force_k() -> Result<(), Box<dyn Error>> {
     let results: Vec<[(f32, usize); K]> = query
         .par_iter()
         .map(|q| unsafe {
-            // let bh = BinaryHeap::from([(F32(f32::MAX), 0)]);
-            let bh = BinaryHeap::default();
+            let bh = BinaryHeap::with_capacity(K);
             core::mem::transmute::<[(F32, usize); K], [(f32, usize); K]>(
                 nearest_k(&data, data.as_ptr(), q, 0, K, bh)
                     .into_sorted_vec()
@@ -52,7 +51,27 @@ fn test_brute_force_k() -> Result<(), Box<dyn Error>> {
         .enumerate()
         .for_each(|(i, q)| assert_eq!(results[i], brute_force(q, &data), "failed on {i}"));
 
-    println!("done with {NQUERY} queries");
+    // Query tree periodic
+    let results: Vec<[(f32, usize); K]> = query
+        .par_iter()
+        .take(NQUERY / 10)
+        .map(|q| unsafe {
+            core::mem::transmute::<[(F32, usize); K], [(f32, usize); K]>(
+                nearest_k_periodic(&data, data.as_ptr(), q, 0, K)
+                    .into_sorted_vec()
+                    .try_into()
+                    .unwrap(),
+            )
+        })
+        .collect();
+
+    // Brute force check results
+    query
+        .iter()
+        // .par_iter()
+        .take(NQUERY / 10)
+        .enumerate()
+        .for_each(|(i, q)| assert_eq!(results[i], brute_force_periodic(q, &data), "failed on {i}"));
 
     Ok(())
 }
@@ -68,12 +87,31 @@ fn random_cp32<const D: usize>(rng: &mut ThreadRng) -> [CP32; D] {
 fn brute_force(q: &[f32; 3], data: &[[CP32; 3]]) -> [(f32, usize); K] {
     let mut best = [(f32::MAX, usize::MAX); K];
     for (d, i) in data.iter().zip(0..) {
-        let dist = squared_euclidean(d, q);
+        let dist_sq = squared_euclidean(d, q);
 
-        if dist < best[K - 1].0 {
-            best[K - 1] = (dist, i);
+        if dist_sq < best[K - 1].0 {
+            best[K - 1] = (dist_sq, i);
 
             best.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+        }
+    }
+
+    best
+}
+
+fn brute_force_periodic(q: &[f32; 3], data: &[[CP32; 3]]) -> [(f32, usize); K] {
+    let mut best = [(f32::MAX, usize::MAX); K];
+    for (d, i) in data.iter().zip(0..) {
+        for img in 0..3_i32.pow(3) {
+            let image = [0_i32, 1, 2]
+                .map(|idx| q[idx as usize] + (((img / 3_i32.pow(idx as u32)) % 3) - 1) as f32);
+
+            let dist_sq = squared_euclidean(d, &image);
+
+            if dist_sq < best[K - 1].0 {
+                best[K - 1] = (dist_sq, i);
+                best.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+            }
         }
     }
 
