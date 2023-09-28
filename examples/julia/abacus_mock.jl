dbea0c6f14196b6a7b17afcad0152359e7653e0e
cavemanloverboy <93507302+cavemanloverboy@users.noreply.github.com>
2023-09-28 12:57:35 -0700

    Update abacus_mock.jl

--- examples/julia/abacus_mock.jl
+++ examples/julia/abacus_mock.jl
@@ -1,7 +1,13 @@
 include("api.jl")
-const libbosque = joinpath(@__DIR__, "..", "..", "target", "release", "libbosque.dylib")
-# on linux:
-# const libbosque = joinpath(@__DIR__, "..", "..", "target", "release", "libbosque.so")
+
+# Import library, conditional on platform
+@static if Sys.isapple()
+    const libbosque = joinpath(@__DIR__, "..", "..", "target", "release", "libbosque.dylib")
+elseif Sys.islinux()
+    const libbosque = joinpath(@__DIR__, "..", "..", "target", "release", "libbosque.so")
+elseif Sys.iswindows()
+    const libbosque = joinpath(@__DIR__, "..", "..", "target", "release", "libbosque.dll")
+end
 
 
 # Compress + Decompress round trip
